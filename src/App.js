a1f1257109298d4f0297ef85ba27c1e59d3fcbbf
@JACOB-Leonard <shinwa.lj@gmail.com>
2022-02-24 09:33:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; //Popup
+import AddProduct from './components/funcComponents';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Salut
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the store !!</h1>
+        <AddProduct/>
       </header>
     </div>
   );
